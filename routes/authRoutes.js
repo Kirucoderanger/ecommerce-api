3c0c2273d1be24d576b6a688d45153f2c8ff26bb
Kirucoderanger <klemu@byupathway.edu>
2025-09-30 03:16:58 +0300

    Login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+//router.post('/login', authController.login);
 // Additional routes like getProfile, updateProfile can be added here
 //router.get('/profile', authController.getProfile);
 //router.put('/profile', authController.updateProfile);
