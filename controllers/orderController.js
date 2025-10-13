58d1d2e76da376691967d470f17bd25b524c1f57
Bettina-M <men23034@byui.edu>
2025-10-13 20:44:59 +0300

    Unit test complete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,26 +1,16 @@
 const orderService = require('../services/orderService');
 const asyncHandler = require('../middleware/asyncHandler') // to handle async errors in express routes
 
-<<<<<<< HEAD
-exports.create = asyncHandler(async (req, res) => {
-  const order = await orderService.createOrder(req.body.userId, req.body.items);
-=======
 exports.create = async (req, res) => {
   const order = await orderService.createOrder(req.body.customerId, req.body.items);
->>>>>>> dac65cd68b96205eeb68bce5632e9335abec89a4
   res.status(201).json(order);
-});
+}
 
-<<<<<<< HEAD
-exports.getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await orderService.getCustomerOrders(req.body.userId);
-=======
 exports.getMyOrders = async (req, res) => {
   const customerId = req.params.id;
   const orders = await orderService.getCustomerOrders(customerId);
->>>>>>> dac65cd68b96205eeb68bce5632e9335abec89a4
   res.json(orders);
-});
+}
 
 exports.getAll = asyncHandler(async (req, res) => {
   const orders = await orderService.getAllOrders();
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 passportConfig(passport);
 */
-
+/*
 // routes
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/auth', require('./routes/authRoutes'));
